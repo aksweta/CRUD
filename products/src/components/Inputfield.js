e70ac8c1df536db74ef9731b4475b431cd49c238
aksweta <aksweta3@gmail.com>
2022-01-28 11:19:58 -0500

    updated inputfield

--- products/src/components/Inputfield.js
+++ products/src/components/Inputfield.js
@@ -3,10 +3,8 @@ import React from "react";
 const Inputfield = ({onchange , type, value, name, label}) => {
     <div>
          {label && <label htmlFor="input-feild">{label}</label>}
-    
-    <input type={} onchange={} value={} name={}/>
+    <input type={type} onchange={onchange} value={value} name={name}/>
     </div>
-
 };
 
 export default Inputfield;
