f1a4eb698672a3094e04790c2bfb1ee0eb997980
aksweta <aksweta3@gmail.com>
2022-01-28 14:47:38 -0500

    starting

--- products/src/App.js
+++ products/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Inputfield from './components/Inputfield.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+          <h1>CRUD PRODUCTS</h1>
+        <Inputfield type="number" label=" UserName :" />
+        <Inputfield type="number" label=" PassWord :" />
+
+
+        <h1>ADD PRODUCTS</h1>
+        <Inputfield type="number" label=" ID :" />
+        <Inputfield type="number" label=" Category :" />
+        <Inputfield type="number" label=" Name :" />
+        <Inputfield type="number" label=" Description :" />
+        <Inputfield type="number" label=" Price :" />
+        <Inputfield type="number" label=" Shipping Charge :" />
+        
+        </div>
   );
 }
 
