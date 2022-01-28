773ad91288a278d3ca7abaff6ee7be2599255a85
aksweta <aksweta3@gmail.com>
2022-01-28 11:10:28 -0500

    create Inputfield

---
+++ products/src/components/Inputfield.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Inputfield = ({onchange , type, value, name, label}) => {
+    <div>
+         {label && <label htmlFor="input-feild">{label}</label>}
+    
+    <input type={} onchange={} value={} name={}/>
+    </div>
+
+};
+
+export default Inputfield;
