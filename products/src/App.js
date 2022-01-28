8807e012077bd371ee7a97046ef4b10eb4de731a
aksweta <aksweta3@gmail.com>
2022-01-28 16:07:52 -0500

    create addproduct

--- products/src/App.js
+++ products/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import Inputfield from './components/Inputfield.js';
 import './App.css';
-import Button from './components/Button.js';
 import Login from './components/Login.js';
-
+import Addproduct from './components/Addproduct.js';
 
 function App() {
   return (
-        <div>
-          
-         <Login />
-
-        <h1>ADD PRODUCTS</h1>
-        <Inputfield type="number" label=" ID :" />
-        <Inputfield type="number" label=" Category :" />
-        <Inputfield type="number" label=" Name :" />
-        <Inputfield type="number" label=" Description :" />
-        <Inputfield type="number" label=" Price :" />
-        <Inputfield type="number" label=" Shipping Charge :" />
-        <Button colour="orange" textcolor="white">Submit On Board</Button>
-        
-        </div>
+    <div>
+      <Login />
+      <Addproduct />
+    </div>
   );
 }
 
