65ca39ecb65824cdf12cf53d1e2586182f4744ce
aksweta <aksweta3@gmail.com>
2022-01-28 15:11:32 -0500

    button done

---
+++ products/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ onClick, children,colour, textcolor }) => {
+    return (
+         <div>
+             <button onClick={onClick} style={{backgroundColor: `${colour}`, color:`${textcolor}`}}>{children}</button>
+         </div>
+    );
+};
+
+export default Button;
