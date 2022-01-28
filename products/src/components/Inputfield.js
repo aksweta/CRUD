abbf1edc64b698d8a5ec807d5e7fcf194c05b235
aksweta <aksweta3@gmail.com>
2022-01-28 11:54:38 -0500

    Fix the issue and fix formatting

--- products/src/components/Inputfield.js
+++ products/src/components/Inputfield.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Inputfield = ({onchange , type, value, name, label}) => {
-    <div>
-         {label && <label htmlFor="input-feild">{label}</label>}
-    <input type={type} onchange={onchange} value={value} name={name}/>
-    </div>
+const Inputfield = ({ onchange , type, value, name, label }) => {
+    return (
+        <div>   
+            {label && <label htmlFor="input-feild">{label}</label>}
+            <input type={type} onchange={onchange} value={value} name={name} />
+        </div>
+    )
 };
 
 export default Inputfield;
