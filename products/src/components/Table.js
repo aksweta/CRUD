2f2e0c10178dffe41fc4f3701329c333d3af170e
aksweta <aksweta3@gmail.com>
2022-01-29 00:34:22 -0500

    tabledone

---
+++ products/src/components/Table.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Table = (props) => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
