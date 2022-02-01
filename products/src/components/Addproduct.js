cae3d82d8cdfe5a5313782ef36954adecb2cc5c3
aksweta <aksweta3@gmail.com>
2022-02-01 07:25:26 -0500

    Conflict solve

--- products/src/components/Addproduct.js
+++ products/src/components/Addproduct.js
@@ -4,37 +4,41 @@ import Button from "./Button";
 import Table from "./Table.js";
 
 const Addproduct = () => {
-     
-    // const [isTableDisplay , setIsTableDisplay] = useState(false);
-        
-      const [product, setproduct] = useState({
-          id: "",
-          category: "",
-          name: "",
-          desc: "",
-          price: "",
-          shipping : ""
-        });
+    
+    const [Product , SetProduct] = useState({
+        ID : '',
+        Category : '',
+        Name : '',
+        Description : '',
+        Price : '',
+        ShippingCharge : ''
+});
+    
 
 
+    const handletext = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        Product[name] = value ;
+        SetProduct(Product);
+    }
 
 
+    const handlesubmit = () => {
+        console.log("Product:" , Product );
+    }
 
     return(
         <div>
         <h1>ADD PRODUCTS</h1>
-        <br />
-        <Inputfield type="number" label=" ID :" name="id" onChange={handletext} />
-        <Inputfield type="number" label=" Category :" />
-        <Inputfield type="number" label=" Name :" />
-        <Inputfield type="number" label=" Description :" />
-        <Inputfield type="number" label=" Price :" />
-        <Inputfield type="number" label=" Shipping Charge :" />
-        <Button colour="firebrick" textcolor="white">Submit On Board</Button>
-
-        <br/>
-         <Table />
-
+        <Inputfield type="number" label=" ID :"  name="ID" onchange={handletext}/>
+        <Inputfield type="number" label=" Category :" name="Category"  onchange={handletext}/>
+        <Inputfield type="number" label=" Name :" name="Name" onchange={handletext}/>
+        <Inputfield type="number" label=" Description :" name="Description"  onchange={handletext}/>
+        <Inputfield type="number" label=" Price :" name="Price" onchange={handletext} />
+        <Inputfield type="number" label=" Shipping Charge :" name="ShippingCharge" onchange={handletext} />
+        <Button colour="orange" textcolor="white" onClick={handlesubmit}>Submit On Board</Button>
         </div>  
     );
 };
